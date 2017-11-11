1df7c7f85a315d33f991abbcfb14fd51919b0fdc
Yan <Yan@MacBook-Pro-de-Yan.local>
2017-11-11 12:57:59 -0500

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,19 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  if Rails.root.join('tmp/caching-dev.txt').exist?
+    config.action_controller.perform_caching = true
+
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
+    }
+  else
+    config.action_controller.perform_caching = false
+
+    config.cache_store = :null_store
+  end
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
