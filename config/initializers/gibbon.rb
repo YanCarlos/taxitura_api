2921fcf577ee901e5d552fe2ea0385e030832f75
Yan <Yan@MacBook-Pro-de-Yan.local>
2017-11-29 19:16:45 -0500

    set api key mailchimp

--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,5 +1,5 @@
-Gibbon::Request.api_key = ENV['MAILCHIMP_API_KEY'] || 'be787f2e0c40fc74ec7289bd95a3e9a1'
+Gibbon::Request.api_key = ENV['MAILCHIMP_API_KEY']
 Gibbon::Request.timeout = 15
 Gibbon::Request.open_timeout = 15
 Gibbon::Request.symbolize_keys = true
-Gibbon::Request.debug = false
+Gibbon::Request.debug = true
