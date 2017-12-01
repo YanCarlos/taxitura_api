4736a0f5336d136bde18fd7b0291a9fba43ce46f
Nikollai Hernandez <nikollaihernandez@gmail.com>
2017-12-01 12:23:47 -0500

    Se actualiza la apariencia del logo principal y los colores del menú para que se visualicen correctamente

--- db/schema.rb
+++ db/schema.rb
@@ -15,28 +15,6 @@ ActiveRecord::Schema.define(version: 20171130171743) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "audits", force: :cascade do |t|
-    t.integer "auditable_id"
-    t.string "auditable_type"
-    t.integer "associated_id"
-    t.string "associated_type"
-    t.integer "user_id"
-    t.string "user_type"
-    t.string "username"
-    t.string "action"
-    t.jsonb "audited_changes"
-    t.integer "version", default: 0
-    t.string "comment"
-    t.string "remote_address"
-    t.string "request_uuid"
-    t.datetime "created_at"
-    t.index ["associated_id", "associated_type"], name: "associated_index"
-    t.index ["auditable_id", "auditable_type"], name: "auditable_index"
-    t.index ["created_at"], name: "index_audits_on_created_at"
-    t.index ["request_uuid"], name: "index_audits_on_request_uuid"
-    t.index ["user_id", "user_type"], name: "user_index"
-  end
-
   create_table "inboxes", force: :cascade do |t|
     t.string "nombre"
     t.string "email"
@@ -101,10 +79,6 @@ ActiveRecord::Schema.define(version: 20171130171743) do
     t.boolean "activo", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "foto_file_name"
-    t.string "foto_content_type"
-    t.integer "foto_file_size"
-    t.datetime "foto_updated_at"
     t.string "foto"
     t.decimal "credito", default: "0.0"
     t.decimal "credito_ganancia", default: "0.0"
