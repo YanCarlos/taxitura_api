d801b52a6729f0932589d6fb615ed7d78a295de2
Nikollai Hernandez <nikollaihernandez@gmail.com>
2017-12-30 17:08:20 -0500

    Información para las graficas de los taxistas

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -17,4 +17,8 @@ module ServicesHelper
     Service.where('info @> ?', {service: {id: id}}.to_json)[0]
   end
 
+  def self.get_last_service id
+    Service.where('info @> ? OR info @> ? OR info @> ? OR info @> ?', {action: "accept"}.to_json, {action: "arrive"}.to_json, {action: "aboard"}.to_json, {action: "cancel"}.to_json).where('info @> ?', {cabman: {id: id}}.to_json).order(created_at: :desc).limit(1)
+  end
+
 end
