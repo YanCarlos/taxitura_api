66f7e9e7af8b1bfd0015172cc6bda9881841cd2e
Nikollai Hernandez <nikollaihernandez@gmail.com>
2018-01-23 17:09:19 -0500

    Se agrega el paquete de bienvenida

--- app/models/recharge.rb
+++ app/models/recharge.rb
@@ -9,4 +9,16 @@ class Recharge < ApplicationRecord
     }
     self.user.update(new_credit)
   end
+
+  before_save do
+  	puts self.to_json
+  	if self.utilidad_taxista == 100 && self.valor_de_carrera == 0
+  		driver = User.find(self.user.id)
+  		if !driver['welcome']
+  			self.user.update({welcome: true})
+  		else
+  			CustomError.send('Anteriormente se asigno el paquete de bienvenida a este taxista.')
+  		end
+  	end
+  end
 end
